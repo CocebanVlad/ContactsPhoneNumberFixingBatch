18707b3f6a044673f4ba31d71a5644206b018169
Vlad Coceban <coceban.vlad@hotmail.com>
2022-02-02 10:40:11 +0200

    first commit

---
+++ ContactsPhoneNumberFixingBatch/GoogleContactRow.cs
@@ -0,0 +1,53 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace ContactsPhoneNumberFixingBatch
+{
+    public class GoogleContactRow
+    {
+        [Index(0)] public string Name { get; set; }
+        [Index(1)] public string GivenName { get; set; }
+        [Index(2)] public string AdditionalName { get; set; }
+        [Index(3)] public string FamilyName { get; set; }
+        [Index(4)] public string YomiName { get; set; }
+        [Index(5)] public string GivenNameYomi { get; set; }
+        [Index(6)] public string AdditionalNameYomi { get; set; }
+        [Index(7)] public string FamilyNameYomi { get; set; }
+        [Index(8)] public string NamePrefix { get; set; }
+        [Index(9)] public string NameSuffix { get; set; }
+        [Index(10)] public string Initials { get; set; }
+        [Index(11)] public string Nickname { get; set; }
+        [Index(12)] public string ShortName { get; set; }
+        [Index(13)] public string MaidenName { get; set; }
+        [Index(14)] public string Birthday { get; set; }
+        [Index(15)] public string Gender { get; set; }
+        [Index(16)] public string Location { get; set; }
+        [Index(17)] public string BillingInformation { get; set; }
+        [Index(18)] public string DirectoryServer { get; set; }
+        [Index(19)] public string Mileage { get; set; }
+        [Index(20)] public string Occupation { get; set; }
+        [Index(21)] public string Hobby { get; set; }
+        [Index(22)] public string Sensitivity { get; set; }
+        [Index(23)] public string Priority { get; set; }
+        [Index(24)] public string Subject { get; set; }
+        [Index(25)] public string Notes { get; set; }
+        [Index(26)] public string Language { get; set; }
+        [Index(27)] public string Photo { get; set; }
+        [Index(28)] public string GroupMembership { get; set; }
+        [Index(29)] public string E_mail1_Type { get; set; }
+        [Index(30)] public string E_mail1_Value { get; set; }
+        [Index(31)] public string E_mail2_Type { get; set; }
+        [Index(32)] public string E_mail2_Value { get; set; }
+        [Index(33)] public string Phone1_Type { get; set; }
+        [Index(34)] public string Phone1_Value { get; set; }
+        [Index(35)] public string Organization1_Type { get; set; }
+        [Index(36)] public string Organization1_Name { get; set; }
+        [Index(37)] public string Organization1_YomiName { get; set; }
+        [Index(38)] public string Organization1_Title { get; set; }
+        [Index(39)] public string Organization1_Department { get; set; }
+        [Index(40)] public string Organization1_Symbol { get; set; }
+        [Index(41)] public string Organization1_Location { get; set; }
+        [Index(42)] public string Organization1_JobDescription { get; set; }
+
+
+    }
+}
---
+++ ContactsPhoneNumberFixingBatch/Program.cs
@@ -0,0 +1,124 @@
+﻿using ContactsPhoneNumberFixingBatch;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+using System.Text;
+
+var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+{
+    HasHeaderRecord = false
+};
+
+var reusableStrBuilder = new StringBuilder();
+
+GoogleContactRow header;
+List<GoogleContactRow> rows;
+
+using (var reader = new StreamReader(@"C:\Users\Vlad\Downloads\contacts.csv"))
+using (var csv = new CsvReader(reader, config))
+{
+    csv.Read();
+    header = csv.GetRecord<GoogleContactRow>();
+    rows = csv.GetRecords<GoogleContactRow>().ToList();
+}
+
+using (var reader = new StreamWriter(@"C:\Users\Vlad\Downloads\contacts.fixed.csv"))
+using (var csv = new CsvWriter(reader, config))
+{
+    csv.WriteRecord(header);
+    csv.NextRecord();
+
+    foreach (var row in rows)
+    {
+        Update(row);
+        csv.WriteRecord(row);
+        csv.NextRecord();
+    }
+}
+
+Console.WriteLine("Done!");
+Console.ReadKey();
+
+void Update(GoogleContactRow row)
+{
+    if (!string.IsNullOrEmpty(row.E_mail1_Value))
+        row.E_mail1_Type = "*";
+
+    if (!string.IsNullOrEmpty(row.E_mail2_Value))
+        row.E_mail2_Type = "";
+
+    if (row.Notes.StartsWith("This contact is read-only") || row.Notes.StartsWith("#NAME?"))
+        row.Notes = "";
+
+    row.Phone1_Type = "";
+    row.Phone1_Value = FixPhones(row.Phone1_Value);
+
+    row.Birthday = "";
+
+    row.GroupMembership = "";
+
+    row.Organization1_Type = "";
+    row.Organization1_YomiName = "";
+    row.Organization1_Department = "";
+    row.Organization1_Symbol = "";
+    row.Organization1_Location = "";
+    row.Organization1_JobDescription = "";
+}
+
+string FixPhones(string rawPhonesStr)
+{
+    if (string.IsNullOrWhiteSpace(rawPhonesStr))
+        return "";
+
+    var delimiter = " ::: ";
+    var phones = rawPhonesStr.Split(delimiter);
+
+    var result = new List<string>();
+
+    foreach (var rawPhone in phones)
+    {
+        var phone = RemoveChars(rawPhone, " -()");
+
+        if (!phone.StartsWith('+'))
+        {
+            if (phone.Length == 11)
+            {
+                // RO number
+                if (phone.StartsWith("40"))
+                    phone = "+" + phone;
+
+                // MD number
+                if (phone.StartsWith("373"))
+                    phone = "+" + phone;
+            }
+            else if (phone.Length == 10)
+            {
+                // RO number
+                if (phone.StartsWith("07"))
+                    phone = "+4" + phone;
+            }
+            else
+            {
+                Console.WriteLine($"Dont know what to do with: {phone}");
+                continue;
+            }
+        }
+
+        result.Add(phone);
+    }
+
+    return string.Join(delimiter, result);
+}
+
+string RemoveChars(string str, string chars)
+{
+    reusableStrBuilder.Clear();
+
+    foreach (var c in str)
+    {
+        if (!chars.Contains(c))
+            reusableStrBuilder.Append(c);
+    }
+
+    return reusableStrBuilder.ToString();
+}
